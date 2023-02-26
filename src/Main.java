5557188775fdafbe828f15f1442aa87a15272512
Antonioni17 <antonioni17@yandex.ru>
2023-02-26 17:18:59 +0300

    correct formula2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+
+        double weightKilo = 120.78;
+        double hightMetr = 1.87;
+        int myBMI = service.calculate(weightKilo, hightMetr);
+        System.out.println(" Ваш  bmi-индекс равен : " + myBMI );
     }
 }
